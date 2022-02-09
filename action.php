8da49e298c5099f0f3904e6e240b868efaa8c7d8
tisumon <tisumonn@gmail.com>
2022-02-09 09:15:36 +0600

    class

---
+++ action.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use App\classes\Home;
+
+if (isset($_GET['pages']))
+{
+    if($_GET['pages'] == 'home')
+    {
+        include 'pages/home.php';
+    }
+}
+
